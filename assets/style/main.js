745df547517f18fc9eeb643166eb979343ef6348
M Zen Comaini <zencomaini@gmail.com>
2022-06-27 23:33:51 +0700

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   about.html
    	modified:   assets/style/main.js
    	modified:   assets/style/style.css
    	modified:   collection.html
    	modified:   index.html
    	deleted:    tutorial.html
    
     Untracked files:
    	articles/

--- assets/style/main.js
+++ assets/style/main.js
@@ -1,10 +1,13 @@
-let bintang = "";
-for(let i = 8; i >= 1; i -- ) {
-    for(let n = 0; n >= 1 - i; n -- ) { 
-        bintang += "*";
-    }
-    bintang += "\n";
-};
+// let bintang = "";
+// for(let i = 8; i >= 1; i -- ) {
+//     for(let n = 0; n >= 1 - i; n -- ) { 
+//         bintang += "*";
+//     }
+//     bintang += "\n";
+// };
 
-console.log(bintang);
+// console.log(bintang);
 
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(json => console.log(json))
